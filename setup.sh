f98986c835c679e231209a5351caedf9c441b57a
yantao <1751852185@qq.com>
2023-12-21 15:27:15 +0800

    安装脚本

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash  
+  
+# 更新软件包列表  
+sudo -E apt update  
+  
+# 安装c开发环境
+sudo -E apt install -y build-essential
+  
+# 安装其他常用的工具  
+sudo -E apt install -y curl wget vim  git tree
+  
+
+# 安装golang开发环境
+wget -O go.tar.gz  https://golang.google.cn/dl/go1.21.5.linux-amd64.tar.gz 
+sudo -E rm -rf /usr/local/go && sudo tar -zxvf go.tar.gz -C /usr/local
+cd 
+mkdir -p go/{bin,pkg,src}
+
+# 设置go环境变量
+sudo -E sh -c 'echo "export GO111MODULE=on" >> /etc/profile'
+sudo -E sh -c 'echo "export GOROOT=/usr/local/go" >> /etc/profile'
+sudo -E sh -c 'echo "export GOPATH=/home/yantao/go" >> /etc/profile'
+sudo -E sh -c 'echo "export GOBIN=\$GOPATH/bin" >> /etc/profile'
+sudo -E sh -c 'echo "export GOPROXY=https://goproxy.cn,direct" >> /etc/profile'
+# 添加path
+sudo -E sh -c 'echo "export PATH=\$PATH:\$GOROOT/bin" >> /etc/profile'
+sudo -E sh -c 'echo "export PATH=\$PATH:\$GOBIN" >> /etc/profile'
+
+# 安装dk11
+sudo -E apt install -y openjdk-11-jdk
