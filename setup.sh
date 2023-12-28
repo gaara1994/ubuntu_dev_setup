31a3744aefb90bececaf7e75f2762ea68d102af2
yantao <1751852185@qq.com>
2023-12-28 20:45:56 +0800

    terminator

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ sudo -E apt update
 # 安装基本开发工具
 sudo -E apt install -y build-essential
 # 安装其他常用的工具  
-sudo -E apt install -y curl wget vim  git tree
+sudo -E apt install -y curl wget vim  git tree terminator
 
 # 安装golang开发环境
 wget -O go.tar.gz  https://golang.google.cn/dl/go1.21.5.linux-amd64.tar.gz 
 sudo -E rm -rf /usr/local/go && sudo tar -zxvf go.tar.gz -C /usr/local
-cd && mkdir -p go1/{bin,pkg,src}
+cd && mkdir -p go/{bin,pkg,src}
 # 设置go环境变量
 sudo -E sh -c 'echo "export GO111MODULE=on" >> /etc/profile'
 sudo -E sh -c 'echo "export GOROOT=/usr/local/go" >> /etc/profile'
